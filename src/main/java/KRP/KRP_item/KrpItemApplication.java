d3a69ab1973fdb1b2d22dc02e9062b88c25bcc92
leesj <jinycap7@gmail.com>
2024-01-23 18:53:02 +0900

    initial Commit

---
+++ src/main/java/KRP/KRP_item/KrpItemApplication.java
@@ -0,0 +1,13 @@
+package KRP.KRP_item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrpItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrpItemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/KRP/KRP_item/KrpItemApplicationTests.java
@@ -0,0 +1,13 @@
+package KRP.KRP_item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrpItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
